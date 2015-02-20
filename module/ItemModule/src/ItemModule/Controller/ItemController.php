41122324700a1f804cb015eb3981d331b0e47dd3
kelios <kelios@inbox.ru>
2015-02-20 18:24:48 +0300

    all project

--- module/ItemModule/src/ItemModule/Controller/ItemController.php
+++ module/ItemModule/src/ItemModule/Controller/ItemController.php
@@ -389,7 +389,7 @@ class ItemController extends AbstractActionController {
         $thumb_big->adaptiveResize(600, 600);
         $thumb_big->save($big_filename);
 
-        $thumb_small->adaptiveResize(140, 140);
+        $thumb_small->adaptiveResize(200, 200);
         $thumb_small->save($small_filename);
 
         unlink($uploadPath . '/' . $name);
